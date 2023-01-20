25372f67f52b4a947f82ccb49b7ab382ad150bc8
Stéphane Menut <smenut@dawan.fr>
2023-01-20 11:09:02 +0100

    fin des services

--- src/Service/Crypto.php
+++ src/Service/Crypto.php
@@ -2,24 +2,32 @@
 
 namespace App\Service;
 
+use Psr\Log\LoggerInterface;
+
 class Crypto
 {
 
     private $cipher = "AES-128-CBC";
-    private $passphrase = "Commentestvotreblanquette?";
+    private $passphrase;
     private $ivlen;
+    private $logger;
 
-    public function __construct()
+    public function __construct(LoggerInterface $logger, string $passphrase)
     {
         $this->ivlen = openssl_cipher_iv_length($this->cipher);
+        $this->logger = $logger;
+        $this->passphrase = $passphrase;
     }
 
     public function encode(string $plaintext): ?string
     {
+        $this->logger->info("Start encode message");
+        $this->logger->info("Passphrase used: {passphrase}", ['passphrase' => $this->passphrase]);
         $iv = openssl_random_pseudo_bytes($this->ivlen);
         $ciphertext_raw = openssl_encrypt($plaintext, $this->cipher, $this->passphrase, OPENSSL_RAW_DATA, $iv);
         $hmac = hash_hmac('sha256', $ciphertext_raw, $this->passphrase, true);
 
+        $this->logger->info("Encoding finished and cipher returned");
         return base64_encode( $iv.$hmac.$ciphertext_raw );
     }
 
